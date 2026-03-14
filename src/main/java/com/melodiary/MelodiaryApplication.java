9d10cecfc41b5b169f3ce23e9627ae47cee901f6
Abhishek Anand <abhishekanand.468@gmail.com>
2026-03-14 15:21:36 +0530

    initial project setup - melodiary spring boot

---
+++ src/main/java/com/melodiary/MelodiaryApplication.java
@@ -0,0 +1,13 @@
+package com.melodiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melodiary/MelodiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melodiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
