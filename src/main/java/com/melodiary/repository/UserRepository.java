cf627b9cf9e6a5101b78aac2d2e89b3c84235552
Abhishek Anand <abhishekanand.468@gmail.com>
2026-03-15 11:51:39 +0530

    Added User entity and User Respository

---
+++ src/main/java/com/melodiary/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.melodiary.repository;
+
+import com.melodiary.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Boolean existsByUsername(String username);
+}
